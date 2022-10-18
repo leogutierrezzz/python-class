6b412111c2d13545a119241dd76a69f25666110c
leogutierrezzz <gutierrez.medeiros12@gmail.com>
2022-10-18 12:27:00 -0400

    Tipos de Dados

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 # Criando o primeiro arquivo para aprender python
 
-print("Olá, Mundo!")
+#print("Olá, Mundo!")
 
-nome = input('Informe o seu nome: ')
-print('O seu nome é: {}'.format(nome))
+#nome = input('Informe o seu nome: ')
+#print('O seu nome é: {}'.format(nome))
+
+# Tipos primitivos
+
+# int
+# bool
+# float
+# str
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+s = n1 + n2
+
+print('A soma de {} e {} vale {}'.format(n1, n2, s))
